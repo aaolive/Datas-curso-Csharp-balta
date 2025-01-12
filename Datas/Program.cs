ddc80e40a18f73ed54c69c4cefec97121b148371
Anderson <anderson.assis@outlook.com>
2025-01-12 18:53:37 -0300

    Projeto inicial

---
+++ Datas/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Datas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+
+        var data = new DateTime();
+        Console.WriteLine(data);
+
+        var formato = "{0:yyyy-MM-dd}";
+
+        Console.WriteLine(string.Format(formato, data)); // formata a data com o modelo de formato
+
+
+        var data2 = DateTime.Now;
+        Console.WriteLine(string.Format("{0:t}", data2)); // so tempo  :t
+
+        var data3 = DateTime.Now;
+        Console.WriteLine(string.Format("{0:d}", data3)); // so data  :d
+
+        Console.WriteLine(string.Format("{0:T}", data3)); // so data  :T tempo longo
+        Console.WriteLine(string.Format("{0:D}", data3)); // so data  :D data longa
+
+        //formato de data universal
+        Console.WriteLine(string.Format("{0:u}", data3)); // data universal do json
+
+        Console.WriteLine(string.Format("{0:r}", data3)); // do javascript
+
+        Console.WriteLine(string.Format("{0:s}", data3)); // do banco de dados mongo
+    }
+}
